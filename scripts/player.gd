a147dd3050ae97f1d686b44dc56cd6c4f1bd90ae
Ayslan-Gamedev <ayslan.fontes@outlook.com>
2024-08-01 14:36:42 -0300

    Add jump ncress

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,24 +1,48 @@
-
 class_name Player
 extends CharacterBody2D
-
 
 @onready var animated_sprite := $AnimatedSprite2D
 var scale_alteration:float = 100
 
 var PAUSED:bool = false
 
+@export_group("Walk")
 @export var SPEED:float = 300.0
+
+@export_group("Jump")
 @export var JUMP_VELOCITY:float = -400.0
-@export var DOUBLE_JUMP: int = 2
+@export var DOUBLE_JUMP:int = 2
 @export var COYOTE_TIME:float = .25
+@export var JUMP_TIME:float = .35
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity:float = ProjectSettings.get_setting("physics/2d/default_gravity")
 var coyote_jump:bool = false
 var jumps:int = 0
+var jump_button:bool = false
 
 @onready var _coyote_timer : Timer
+@onready var _jump_timer : Timer
+
+
+func _stop_jump_timer():
+	if _jump_timer != null:
+		_jump_timer.stop()
+		remove_child(_jump_timer)
+	
+	_jump_timer = null
+
+
+func _start_jump_timer():
+	if _jump_timer != null:
+		_stop_jump_timer()
+
+	_jump_timer = Timer.new()
+	_jump_timer.wait_time = JUMP_TIME
+	_jump_timer.one_shot = true
+	add_child(_jump_timer)
+	_jump_timer.start()
+
 
 func _stop_coyote_timer():
 	if _coyote_timer != null:
@@ -73,18 +97,30 @@ func _jump_controller():
 	else:
 		jumps = 0
 		coyote_jump = false
+
 	
 	# calculate if player can jump by coyote time or in floor or extra-jump
 	var can_jump:bool = ((_coyote_timer != null and _coyote_timer.time_left > 0) or 
 		is_on_floor() or (jumps != 0 and jumps < DOUBLE_JUMP))
-
-	# Handle jump
+	
+	if _coyote_timer != null:
+		print(_coyote_timer.time_left)
+	
 	if Input.is_action_just_pressed("ui_accept"):
-		if can_jump:
-			velocity.y = JUMP_VELOCITY
-			
+		if can_jump:	
 			_stop_coyote_timer()
 			coyote_jump = true
 			can_jump = false
 			jumps += 1
+
+			_start_jump_timer()
+			jump_button = true
+
+	if Input.is_action_just_released("ui_accept") or (_jump_timer != null and _jump_timer.time_left <= 0):
+		jump_button = false
+		_stop_jump_timer()
 	
+	# Handle jump
+	if jump_button:
+		velocity.y = JUMP_VELOCITY
+		
