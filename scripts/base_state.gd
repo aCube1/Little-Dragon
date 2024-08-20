f09fb4d60f793c740c52fe3474f35770b2d220d4
aCube <player559001@gmail.com>
2024-08-20 01:15:16 -0300

    Fix StateMachine; Add documentation. Fix StateMachine not changing current state. Add documentation for new Nodes.

--- scripts/base_state.gd
+++ scripts/base_state.gd
@@ -1,14 +1,15 @@
 class_name BaseState
 extends Node
-## Base state node template for StateMachine
-##
-## This node only purpose is to serve as an base for new
+## Base state node template for StateMachine. [br]
+## This node only purpose is to serve as a base for new
 ## states behaviours.
 
-
+@warning_ignore("unused_signal")
 signal registered ## Called when the state has added to a StateMachine
+@warning_ignore("unused_signal")
 signal completed(next: String, msg: Dictionary)
 
+var machine: StateMachine
 
 func _enter(_msg: Dictionary) -> void:
 	pass
--- scripts/state_machine.gd
+++ scripts/state_machine.gd
@@ -1,5 +1,6 @@
 class_name StateMachine
 extends Node
+## A simple FSM(Finite State Machine) Node to change multiple states from an object
 
 signal state_changed(next: String)
 
@@ -13,24 +14,46 @@ var _state: BaseState
 var _next_state := ""
 var _msg: Dictionary
 
-## Setup internal states Dictionary with the states description
-##
-## Set each state Name, NodePath and Transitions:
-## "name": Unique state key on the Dictionary
-## "path": Path relative to the StateMachine node on the SceneTree
-## "change_to": Array of strings containing the state names
-## 	the state can transition to
+
+func _process(delta: float) -> void:
+	# Check there is a pending state to change to
+	if not _next_state.is_empty() and _next_state != current_state:
+		if _states.has(_next_state):
+			_set_state(_next_state)
+		else:
+			push_warning("State '%s' is not registered" % _next_state)
+		_next_state = ""
+
+	if _state != null:
+		_state._update(delta);
+
+
+func _physics_process(delta: float) -> void:
+	if _state != null:
+		_state._physics_update(delta)
+
+
+## Setup internal states Dictionary with the states description.
+## The each dictionary from [param states] must have the following keys: [br]
+## [code]"name"[/code]: [String] [br]
+## [code]"path"[/code]: [NodePath] [br]
+## [code]"change_to"[/code]: [PackedStringArray]
 func setup(states: Array[Dictionary], init: String) -> void:
-	for desc in states:
+	for desc: Dictionary in states:
 		if not desc.has_all(["name", "path", "change_to"]):
 			push_error("Invalid state provided at setup: %s" % desc)
 			continue
 
-		add_state(desc["name"], desc["path"], desc["transitions"])
+		add_state(desc["name"], desc["path"], desc["change_to"])
 
 	_set_state(init)
 
 
+## Register a new state to the machine: [br]
+## [param name]: Unique state key on the Dictionary [br]
+## [param path]: Path relative to the StateMachine node on the SceneTree [br]
+## [param change_to]: Array of strings containing the state names the state can
+## transition to
 func add_state(state_name: String, path: NodePath, change_to: PackedStringArray) -> void:
 	if _states.has(name):
 		push_warning("StateMachine already has state named: %s" % name)
@@ -42,7 +65,8 @@ func add_state(state_name: String, path: NodePath, change_to: PackedStringArray)
 		return
 
 	state.owner = root
-	state.emit("registered")
+	state.machine = self
+	state.registered.emit()
 	_states[state_name] = { "path": path, "change_to": change_to }
 
 
@@ -52,7 +76,7 @@ func _set_state(state: String) -> void:
 		return # Cannot change to an invalid state
 
 	if _state != null:
-		_state._exits()
+		_state._exit()
 		previous_state = current_state
 
 	current_state = state
@@ -69,7 +93,7 @@ func _on_state_completed(next: String, msg: Dictionary) -> void:
 
 	# If the next state was registered, transition to it, if not,
 	# go to the previous state.
-	var change_to: PackedStringArray = _states[_state].change_to
+	var change_to: PackedStringArray = _states[current_state].change_to
 	if change_to.has(next):
 		_next_state = next
 	elif not previous_state.is_empty():
